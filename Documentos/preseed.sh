af3281e31fe437d3800e3e2641a841e8e37c61d5
constrict0r <constrict0r@protonmail.com>
2020-01-28 19:03:09 -0600

    Preseed last question

--- Documentos/preseed.sh
+++ Documentos/preseed.sh
@@ -8,7 +8,7 @@
 # Replace with your data and run on a terminal.
 ansible localhost -m include_role -a name=constrict0r.iso -K -e \
     'username=constrict0r userpass=1234 rootpass=1234 device=sda preseed=true \
-    preseed_wireless=true preseed_partitioning=true preseed_last_question=false \
+    preseed_wireless=true preseed_partitioning=true preseed_last_question=true \
     preseed_custom=/home/constrict0r/Documentos/madvillain.yml networkname="MY NETWORK" \
     networkpass="my-network-pass" hostname="latveria" domain="amanita"'
 
